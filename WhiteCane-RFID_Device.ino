23b367ccc527b2766dd3a730430c656b0a319ff1
Gyudong-Kim <dong3955@gmail.com>
2023-04-16 18:43:30 +0900

    [FEAT] 읽기, 쓰기 모드 구분

--- WhiteCane-RFID_Device.ino
+++ WhiteCane-RFID_Device.ino
@@ -7,7 +7,8 @@
 
 MFRC522 mfrc522(SDA, RST); // MFRC522 객체 생성
 
-int flag = 0; // 0: Nothing, 1: Writing
+int mode = 0; // 0: Read, 1: Write
+String data = ""; // 장소 데이터 저장
 
 void setup() {
   Serial.begin(9600); // Initialize serial communications with the PC
@@ -18,11 +19,73 @@ void setup() {
 void loop() {
   // data from App
   if(Serial.available()) {
-    flag = 1;
     process_message();
   }
 
-  if(flag == 0) {
-    rfid_reader();
+  // RFID
+  // 키 준비 (초기 세팅은 FFFFFFFFFFFFh로 되어 있음)
+  MFRC522::MIFARE_Key key;
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+
+  // 새 TAG 찾기
+  if (! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // ID 값이 정상인지 CHK'
+  if (! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  /*
+  // RFID Tag의 UID 출력
+  Serial.print("Card UID:");
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  }
+  // PICC 카드의 Type을 출력
+  Serial.print(" PICC type: "); // Dump PICC type
+  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+  Serial.println(mfrc522.PICC_GetTypeName(piccType));
+  */
+
+  if(mode == 0) {
+    rfid_reader(key);
+  } else {
+    rfid_writer(key, data);
+  }
+
+  Serial.println();
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+}
+
+void process_message() {
+  StaticJsonDocument<64> doc;
+  String buffer;
+
+  while(Serial.available()) {
+    char data = Serial.read();
+    if(data == '\n') { // read just 1 line
+      break;
+    } else {
+      buffer = buffer + data;
+    }
+    delay(10);
+  }
+  Serial.println();
+  Serial.println("Got from SerialPort");
+  Serial.println(buffer);
+
+  deserializeJson(doc, buffer);
+
+  // mode and data CHK'
+  if(doc["mode"] == "w") {
+    String location = doc["location"];
+    data = location;
+    mode = 1; // 쓰기 모드로 변경
   }
 }
