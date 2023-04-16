44837ff97e7af6fcfddaec8df9d885ae82c6113b
Gyudong-Kim <dong3955@gmail.com>
2023-04-16 18:41:40 +0900

    [FEAT] PCD 인증 문제 해결

--- RFID_Writer.ino
+++ RFID_Writer.ino
@@ -1,30 +1,31 @@
-void process_message() {
-  StaticJsonDocument<64> doc;
-  String buffer;
+void rfid_writer(MFRC522::MIFARE_Key key, String data) {
+  MFRC522::StatusCode status; // 상태 코드 입력 변수
 
-  while(Serial.available()) {
-    char data = Serial.read();
-    if(data == '\n') { // read just 1 line
-      break;
+  byte buffer[data.length() + 1];
+  data.getBytes(buffer, data.length() + 1);
+  int size = sizeof(buffer);
+
+  for(byte block = 1; block < 3; block++) {
+    // PCD 인증
+    status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_B, block, &key, &(mfrc522.uid));
+    if (status != MFRC522::STATUS_OK) {
+      // Serial.print("PCD_Authenticate() failed: ");
+      // Serial.println(mfrc522.GetStatusCodeName(status));
+      return;
     } else {
-      buffer = buffer + data;
+      // Serial.println("PCD_Authenticate() success: ");
     }
-    delay(10);
+
+    // Write block
+    status = mfrc522.MIFARE_Write(block, &buffer[block * 16 - 16], 16);
+    if (status != MFRC522::STATUS_OK) {
+      // Serial.print("MIFARE_Write() failed: ");
+      // Serial.println(mfrc522.GetStatusCodeName(status));
+      return;
+    } else {
+      // Serial.println("MIFARE_Write() success: ");
+    }
   }
-  Serial.println("Got from SerialPort");
-  Serial.println(buffer);
-  Serial.println();
 
-  deserializeJson(doc, buffer);
-
-  // Mode CHK'
-  if(doc["mode"] == "w") {
-    String location = doc["location"];
-    Serial.println("Location: " + location);
-    rfid_writer(location);
-  }
-}
-
-void rfid_writer(String location) {
-  
+  mode = 0; // 읽기 모드로 변경
 }
